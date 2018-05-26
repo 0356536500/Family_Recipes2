9d56527efdf88e4669fb0436f04fe963154d487c
ronginat <ronginat51@gmail.com>
2018-05-26 14:13:01 +0300

    Initial commit

---
+++ app/src/main/java/com/myapps/ron/family_recipes/Contact.java
@@ -0,0 +1,61 @@
+package com.myapps.ron.family_recipes;
+
+/**
+ * Created by ravi on 16/11/17.
+ */
+
+public class Contact {
+    String title;
+    String image = "https://api.androidhive.info/json/images/keanu.jpg";
+    String author;
+    String genre;
+    String read;
+
+
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getRead() {
+        return read;
+    }
+}
+
+/*
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
+*/
